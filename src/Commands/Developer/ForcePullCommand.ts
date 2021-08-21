e3ca577504298fb27bd7b4756c60662e4d20f09f
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-08-21 23:27:40 +0530

    pull

---
+++ src/Commands/Developer/ForcePullCommand.ts
@@ -0,0 +1,23 @@
+import { Message, Util } from "discord.js";
+import Command from "../../Struct/Command";
+import { exec } from "child_process"
+export default class EvalCommand extends Command{
+    constructor(){
+        super("forcepull",{
+            aliases:["pull","force-pull"],
+            ownerOnly: true,
+            description: "Pull and restart",
+        })
+    }
+    exec(message: Message){
+        exec("cd /root/Jorge && git pull && pm2 restart 0",(stdout,error)=>{
+            message.reply({
+                embeds:[
+                    this.client.embed({
+                        description: `Output: \`\`\`\n${stdout || error}\`\`\``
+                    },message)
+                ]
+            })
+        })
+    }
+} 
