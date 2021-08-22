fe22ce985cb57e902eee4ee8c769bf97625996a5
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-08-22 22:32:20 +0530

    Marvel

---
+++ src/Commands/Fun/MarvelCommand.ts
@@ -0,0 +1,30 @@
+import { DataResolver, Message } from "discord.js";
+import { Marvel } from "../../interfaces/Marvel";
+import ExCommand from "../../Struct/Command";
+import fetch from "node-fetch";
+import { resourceLimits } from "worker_threads";
+export default class MarvelCommand extends ExCommand{
+    constructor(){
+        super("marvel",{
+            aliases: ["marvel"],
+            args:[{
+                id: "query",
+                type: "string",
+                match: "rest",
+                default: "Iron Man"
+            }]
+        })
+    }
+    async exec(message: Message,{ query }: {query: string}){
+        const { Marvel } = this.client.config
+        const { data } = await (await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${encodeURIComponent(query)}&apikey=${Marvel}`)).json() as Marvel
+        const embed = this.client.embed({},message)
+        if(!data.total) embed.setTitle("Not found")
+        else{
+            const result = data.results[0]
+            embed.setTitle(result.name).setDescription(result.description || "No description provided").setThumbnail(`${result.thumbnail.path}.${result.thumbnail.extension}`).addField("Number of comics appeared in",result.comics.available.toString())
+            const exmpl = this.client.trimArray(result.comics.items.map(i => i.name).reverse(),10)
+            embed.addField("Names of comics character appeared in",exmpl)
+        }
+    }
+}
