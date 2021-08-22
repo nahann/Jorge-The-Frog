e5435a2844a4083822d892b2cbbf4805cc68f4fe
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-08-22 22:37:52 +0530

    testy

--- src/Commands/Fun/MarvelCommand.ts
+++ src/Commands/Fun/MarvelCommand.ts
@@ -17,12 +17,12 @@ export default class MarvelCommand extends ExCommand{
     }
     async exec(message: Message,{ query }: {query: string}){
         const { Marvel } = this.client.config
-        const { data } = await (await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${encodeURIComponent(query)}&apikey=${Marvel}`)).json() as Marvel
-        console.log(data)
+        const json = await (await fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${encodeURIComponent(query)}&apikey=${Marvel}`)).json() as Marvel
+        console.log(json)
         const embed = this.client.embed({},message)
-        if(!data.total) embed.setTitle("Not found")
+        if(!json.data.total) embed.setTitle("Not found")
         else{
-            const result = data.results[0]
+            const result = json.data.results[0]
             embed.setTitle(result.name).setDescription(result.description || "No description provided").setThumbnail(`${result.thumbnail.path}.${result.thumbnail.extension}`).addField("Number of comics appeared in",result.comics.available.toString())
             const exmpl = this.client.trimArray(result.comics.items.map(i => i.name).reverse(),10)
             embed.addField("Names of comics character appeared in",exmpl)
