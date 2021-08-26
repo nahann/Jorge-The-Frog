f2df80633dd7f2b4dc109982d2b119451c32f29b
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-08-26 12:02:38 +0530

    dist bruh

--- dist/Functions/Embed.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.embed = void 0;
-const discord_js_1 = require("discord.js");
-const embed = (data, message) => {
-    return new discord_js_1.MessageEmbed(data).setTimestamp().setColor("RANDOM").setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL());
-};
-exports.embed = embed;
