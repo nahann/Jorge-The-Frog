c9d78e1d8761fca9509bd8c9e7b6730161851228
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-08-27 20:37:36 +0530

    fix

--- src/Listeners/Guild/messageCreate.ts
+++ src/Listeners/Guild/messageCreate.ts
@@ -13,7 +13,7 @@ export default class MessageCreateEvent extends Listener{
         if(message.channel.id == "852783453606248468" && !message.content.startsWith("//ignore")) message.crosspost()
         const { db } = this.client
         const schema = await db.load("chatbot")
-        if(schema.findOne({ channelId: message.channel.id })){
+        if(await schema.findOne({ channelId: message.channel.id })){
             message.util?.reply(await chat(message.content))
         }
     }
