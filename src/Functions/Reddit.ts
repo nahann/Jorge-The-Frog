6169d187be625c057df9e57c5ca93cc8ca3f3f0f
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-10-13 18:49:56 +0530

    ok so reddit likes being a dick

--- src/Functions/Reddit.ts
+++ src/Functions/Reddit.ts
@@ -5,10 +5,14 @@ import fetch from "node-fetch"
 export const reddit: RedditFunction = async(query)=>{
     const subreddit: Subreddit = await (await fetch(`https://reddit.com/r/${query}.json`)).json()
     if(!subreddit.data?.children?.length) return { message: "Not found", error: 404 }
-    const pst: Reddit = (await (await fetch(`https://reddit.com/r/${query}/random.json`)).json())[0]
+    const pst: Reddit | Reddit[] = (await (await fetch(`https://reddit.com/r/${query}/random.json`)).json())
+    let post = pst as Reddit
+    if(pst.constructor == Array){
+        post = pst[0]
+    }
     console.log(pst)
-    if(pst.data.children[0].data.over_18) return { message: "NSFW not allowed" }
-    if(pst.error) return pst
-    const { title, selftext: description,ups: upvotes,downs: downvotes, upvote_ratio, url_overridden_by_dest: img, url, author } = pst.data.children[0].data
+    if(post.data.children[0].data.over_18) return { message: "NSFW not allowed" }
+    if(post.error) return post
+    const { title, selftext: description,ups: upvotes,downs: downvotes, upvote_ratio, url_overridden_by_dest: img, url, author } = post.data.children[0].data
     return { title, description, upvote_ratio, upvotes, downvotes, img, url, author }
 }
