5f45111bb4d545d3d37275ad3963554bceae3e20
Lesiiii <72552172+lesigit@users.noreply.github.com>
2021-10-13 18:42:20 +0530

    why is this so fucking annoyin

--- src/Functions/Reddit.ts
+++ src/Functions/Reddit.ts
@@ -4,8 +4,9 @@ import { Subreddit } from "../interfaces/subreddit"
 import fetch from "node-fetch"
 export const reddit: RedditFunction = async(query)=>{
     const subreddit: Subreddit = await (await fetch(`https://reddit.com/r/${query}.json`)).json()
-    if(!subreddit.data.children.length) return { message: "Not found", error: 404 }
+    if(!subreddit.data?.children?.length) return { message: "Not found", error: 404 }
     const pst: Reddit = (await (await fetch(`https://reddit.com/r/${query}/random.json`)).json())[0]
+    console.log(pst)
     if(pst.data.children[0].data.over_18) return { message: "NSFW not allowed" }
     if(pst.error) return pst
     const { title, selftext: description,ups: upvotes,downs: downvotes, upvote_ratio, url_overridden_by_dest: img, url, author } = pst.data.children[0].data
